7dedaa559761cf11adfbb19c829b26d110dd3902
Mathias Lau Kjeldsen <63905371+Mathiaslk3@users.noreply.github.com>
2024-02-21 17:52:55 +0100

    Added Headless UI and Heroicons

---
+++ frontend/src/components/HeaderBackup.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+
+//#region Page import
+import Home from '../pages/Home';
+import Renting from '../pages/Renting';
+import Login from '../pages/Login';
+//#endregion
+
+
+const navigation =[
+  {title: 'Forside', href: '/'},
+  {title: 'Udlejning', href: '/Renting'},
+  {title: 'Hold', href: '/Enrollment'},
+  {title: 'Login', href: '/Login'},
+]
+
+
+
+
+function Header(){
+  return (
+    <>
+      <Router>
+        {/* Link bliver brug til navigation på siden */}
+        {navigation.map((item) =>(
+        <Link to={item.href}>{item.title}</Link>        
+        ))}
+
+        <Routes>
+        {/* Route tjekker hvilket URL du er på, hvis du er på URL'en "/" vil den vise den component som høre til, i dette tilfælde Home */}
+          <Route path='/' element={<Home/>}/>
+          <Route path='/Renting' element={<Renting/>}/>
+          <Route path='/Login' element={<Login/>}/>
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default Header;
